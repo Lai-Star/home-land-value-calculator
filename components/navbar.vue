fca09a93214001f6d7368d4eb1b1e97bd258233f
Lai-Star <mclaistar@gmail.com>
2022-11-22 13:40:52 +0100

    feat: add navbar font and change design

--- components/navbar.vue
+++ components/navbar.vue
@@ -5,14 +5,14 @@
         <nav class="flex justify-between bg-transparent  text-white w-screen">
           <div class="px-5 xl:px-12 py-6 flex w-full items-center">
             <!-- Nav Links -->
-            <ul class="hidden md:flex px-4 mx-auto font-semibold space-x-52" >
-              <li><a class="hover:text-gray-200" href="/">HOME</a></li>
-              <li><a class="hover:text-gray-200" href="/about">MODEL</a></li>
+            <ul class="hidden md:flex px-4 mx-auto space-x-52 ul" >
+              <li><a class="nlink" href="/">HOME</a></li>
+              <li><a class="nlink" href="/model">MODEL</a></li>
 
-              <img class="logo" src="~/assets/img/logo.png" alt="Tailwindcss Navigation" width="50" height="50">
+              <a class="nlog" href="#"><img class="logo" src="~/assets/img/logo.png" alt="Tailwindcss Navigation" width="50" height="50"></a>
 
-              <li><a class="hover:text-gray-200" href="/about">EVALUATION</a></li>
-              <li><a class="hover:text-gray-200" href="/about">ABOUT</a></li>
+              <li><a class="nlink" href="/evaluation">EVALUATION</a></li>
+              <li><a class="nlink" href="/about">ABOUT</a></li>
             </ul>
 
             <!-- Header Icons -->
@@ -87,6 +87,12 @@
 </template>
 
 <style>
+@font-face {
+  font-family: "Roc Grotesk";
+  src: local("Roc Grotesk"),
+  url('../assets/fonts/Fontspring-DEMO-rocgroteskextrawide-medium.otf') format("truetype");
+}
+
 .dropdown:hover .dropdown-menu {
   display: block;
 }
@@ -101,6 +107,118 @@
 .logo:hover{
   transform: scale(-1.01);
   transition: 1s;
+}
+
+.ul{
+  font-family: "Roc Grotesk","Segoe UI";
+  font-size: 16px;
+  text-transform: uppercase;
+  text-align: center;
+}
+
+.ul * {
+  box-sizing: border-box;
+  transition: all .35s ease;
+}
+
+.ul li {
+  display: inline-block;
+  margin: .5em 1em;
+  padding: 0;
+}
+
+.ul .nlink {
+  padding: .5em .8em;
+  position: relative;
+  text-decoration: none;
+  font-size: 20px;
+}
+
+.ul .nlink::before,
+.ul .nlink::after {
+  content: '';
+  height: 14px;
+  width: 14px;
+  position: absolute;
+  transition: all .35s ease;
+  opacity: 0;
+}
+
+.ul .nlink::before {
+  content: '';
+  right: 0;
+  top: 0;
+  border-top: 3px solid #28c0b3;
+  border-right: 3px solid #2a726c;
+  transform: translate(-100%, 50%);
+}
+
+.ul .nlink:after {
+  content: '';
+  left: 0;
+  bottom: 0;
+  border-bottom: 3px solid #2a726c;
+  border-left: 3px solid #28c0b3;
+  transform: translate(100%, -50%)
+}
+
+.ul .nlink:hover:before,
+.ul .nlink:hover:after{
+  transform: translate(0,0);
+  opacity: 1;
+}
+
+.ul .nlink:hover {
+  color: #02ffff;
+}
+
+/* LOGO */
+
+:root {
+  --reco-logo-linka-color: #FFFFFF;
+  --reco-logo-vypln-color: #FFFFFF;
+  --anim-Logo-linka: 3.3s;
+  --anim-opacity_Logo_vypln: .9s;
+  --anim-opacity_Logo_vypln-delay: .9s;
+}
+.logo-anim-wrapper {
+  position: relative;
+  display: flex;
+  width: 45px;
+  height: 53px;
+}
+.logo-anim-wrapper svg { position: absolute; }
+
+#Logo_tvar { opacity: 1;}
+.reco-logo-path{fill:transparent;stroke:var(--reco-logo-linka-color);}
+
+#Logo_vypln { opacity: 0; }
+.lt0{fill:var(--reco-logo-vypln-color);}
+
+
+
+.reco-logo-path {
+  stroke-dasharray: 0,990;
+  stroke-dashoffset: 0;
+  animation: anim-Logo-linka var(--anim-Logo-linka) cubic-bezier(.19,.74,.86,.37) forwards;
+}
+@keyframes anim-Logo-linka {
+  0% {opacity: 1;}
+  100% {
+    opacity: 0;
+    stroke-dasharray: 20,0;
+    stroke-dashoffset: 990;
+  }
+}
+
+#Logo_vypln {
+  opacity: 0;
+  animation: anim-opacity_Logo_vypln var(--anim-opacity_Logo_vypln) cubic-bezier(.99,.06,.88,.58) forwards;
+  animation-delay: var(--anim-opacity_Logo_vypln-delay);
+}
+@keyframes anim-opacity_Logo_vypln {
+  0% {opacity: 0;}
+  100% {opacity: 1;}
 }
 
 </style>
