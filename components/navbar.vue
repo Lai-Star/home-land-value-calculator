81c83e4a7814f7b2c2dfe445ce120f2ab9aec60d
Lai-Star <mclaistar@gmail.com>
2022-11-22 15:49:28 +0100

    feat: change navbar and menu

--- components/navbar.vue
+++ components/navbar.vue
@@ -15,70 +15,50 @@
               <li><a class="nlink" href="/about">ABOUT</a></li>
             </ul>
 
-            <!-- Header Icons -->
-            <!-- <div class="hidden xl:flex items-center space-x-5 items-center">
-              <a class="hover:text-gray-200" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="blue">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                </svg>
-              </a>
-              <a class="flex items-center hover:text-gray-200" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="green">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                </svg>
-                <span class="flex absolute -mt-5 ml-4">
-              <span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
-                <span class="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
-                </span>
-              </span>
-              </a>
-              <a class="flex items-center hover:text-gray-200" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="red">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-              </a>
-
-            </div> -->
           </div>
-          <!-- Responsive navbar -->
-          <!--<a class="xl:hidden flex mr-6 items-center" href="#">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="yellow">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-            </svg>
-            <span class="flex absolute -mt-5 ml-4">
-          <span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
-          <span class="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
-          </span>
-        </span>
-          </a>-->
-          <!--button -->
-         <!-- <div class="p-10">
-            <div class="dropdown inline-block relative">
-              <button class="bg-transparent text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="orange">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                </svg>
-              </button>
-              <ul class="dropdown-menu absolute hidden text-gray-700 pt-1">
-                <li class=""><a class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">One</a></li>
-                <li class=""><a class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Two</a></li>
-                <li class=""><a class="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Three is the magic number</a></li>
-              </ul>
-            </div>
-          </div> -->
+
 
           <div class="xl:hidden flex mr-6 items-center">
-            <div class="dropdown inline-block relative">
-              <button class="bg-transparent text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="orange">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                </svg>
-              </button>
-              <ul class="dropdown-menu absolute hidden text-gray-700 pt-1">
+            <div class="dropdown antialiased float-right bg-transparent w-screen">
+
+              <svg @click="hidden('menu-button')"
+                xmlns="http://www.w3.org/2000/svg"
+                id="menu-button"
+                class="h-6 w-6 cursor-pointer md:hidden block"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+
+              <div class="hidden w-full md:flex md:items-center md:w-auto" id="menu">
+                <ul class="pt-4 text-base text-gray-700 md:flex md:justify-between md:pt-0">
+                  <li>
+                    <a class="md:p-4 py-2 block hover:text-sky-400 text-white" href="/">HOME</a>
+                  </li>
+                  <li>
+                    <a class="md:p-4 py-2 block hover:text-sky-400 text-white"  href="/model">MODEL</a>
+                  </li>
+                  <li>
+                    <a class="md:p-4 py-2 block hover:text-sky-400 text-white" href="/evaluation">EVALUATION</a>
+                  </li>
+                  <li>
+                    <a class="md:p-4 py-2 block hover:text-sky-400 text-white" href="/about">ABOUT</a>
+                  </li>
+                </ul>
+              </div>
+
+             <!-- <ul class="dropdown-menu absolute hidden text-gray-700 pt-1">
                 <li class=""><a class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">One</a></li>
                 <li class=""><a class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Two</a></li>
                 <li class=""><a class="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Three is the magic number</a></li>
-              </ul>
+              </ul> -->
             </div>
           </div>
         </nav>
@@ -173,56 +153,19 @@
 }
 
 /* LOGO */
-
-:root {
-  --reco-logo-linka-color: #FFFFFF;
-  --reco-logo-vypln-color: #FFFFFF;
-  --anim-Logo-linka: 3.3s;
-  --anim-opacity_Logo_vypln: .9s;
-  --anim-opacity_Logo_vypln-delay: .9s;
-}
-.logo-anim-wrapper {
-  position: relative;
-  display: flex;
-  width: 45px;
-  height: 53px;
-}
-.logo-anim-wrapper svg { position: absolute; }
-
-#Logo_tvar { opacity: 1;}
-.reco-logo-path{fill:transparent;stroke:var(--reco-logo-linka-color);}
-
-#Logo_vypln { opacity: 0; }
-.lt0{fill:var(--reco-logo-vypln-color);}
-
-
-
-.reco-logo-path {
-  stroke-dasharray: 0,990;
-  stroke-dashoffset: 0;
-  animation: anim-Logo-linka var(--anim-Logo-linka) cubic-bezier(.19,.74,.86,.37) forwards;
-}
-@keyframes anim-Logo-linka {
-  0% {opacity: 1;}
-  100% {
-    opacity: 0;
-    stroke-dasharray: 20,0;
-    stroke-dashoffset: 990;
-  }
-}
-
-#Logo_vypln {
-  opacity: 0;
-  animation: anim-opacity_Logo_vypln var(--anim-opacity_Logo_vypln) cubic-bezier(.99,.06,.88,.58) forwards;
-  animation-delay: var(--anim-opacity_Logo_vypln-delay);
-}
-@keyframes anim-opacity_Logo_vypln {
-  0% {opacity: 0;}
-  100% {opacity: 1;}
-}
-
 </style>
 
-<script>
 
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+    hidden(x) {
+      document.querySelector('#menu').classList.toggle("hidden");
+    }
+  }
+};
 </script>
---
+++ components/navbar2.vue
@@ -0,0 +1,81 @@
+<template>
+<div
+  class="
+      antialiased
+      bg-gradient-to-r
+      from-pink-300
+      via-purple-300
+      to-indigo-400
+    "
+>
+   <!-------------------- ---------->
+    <svg @click="hidden('menu-button')"
+      xmlns="http://www.w3.org/2000/svg"
+      id="menu-button"
+      class="h-6 w-6 cursor-pointer md:hidden block"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M4 6h16M4 12h16M4 18h16"
+      />
+    </svg>
+
+    <div class="hidden w-full md:flex md:items-center md:w-auto" id="menu">
+      <ul
+        class="
+              pt-4
+              text-base text-gray-700
+              md:flex
+              md:justify-between
+              md:pt-0"
+      >
+        <li>
+          <a class="md:p-4 py-2 block hover:text-purple-400" href="#"
+          >Features</a
+          >
+        </li>
+        <li>
+          <a class="md:p-4 py-2 block hover:text-purple-400" href="#"
+          >Pricing</a
+          >
+        </li>
+        <li>
+          <a class="md:p-4 py-2 block hover:text-purple-400" href="#"
+          >Customers</a
+          >
+        </li>
+        <li>
+          <a class="md:p-4 py-2 block hover:text-purple-400" href="#"
+          >Blog</a
+          >
+        </li>
+        <li>
+          <a
+            class="md:p-4 py-2 block hover:text-purple-400 text-purple-500"
+            href="#"
+          >Sign Up</a
+          >
+        </li>
+      </ul>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+    hidden(x) {
+      document.querySelector('#menu').classList.toggle("hidden");
+    }
+  }
+};
+</script>
