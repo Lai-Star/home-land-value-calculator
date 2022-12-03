1e893b82a248c66bc395aa7a11fbfee2d297487f
Lai-Star <mclaistar@gmail.com>
2022-12-03 09:23:15 +0100

    fix: margin top/bottom

--- components/hero2.vue
+++ components/hero2.vue
@@ -8,7 +8,7 @@
               <h1 class="mt-6 text-4xl font-normal text-white sm:mt-10 sm:text-5xl lg:text-6xl xl:text-8xl"><span class="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-purple-500">R Real Estate</span> Evaluation</h1>
               <p class="max-w-lg mt-4 text-xl font-normal text-gray-400 sm:mt-8">Real estate valuations and property insights based on Big Data analytics and artificial intelligence.
                 Combines mathematical or statistical modeling with databases of existing properties and transactions to calculate real estate values</p>
-              <div class="relative inline-flex items-center justify-center my-32 sm:mt-12 group">
+              <div class="relative inline-flex items-center justify-center my-8 sm:mt-12 group"> <!-- my-32 -->
                 <div class="absolute transition-all duration-200 rounded-full -inset-px bg-gradient-to-r from-cyan-500 to-purple-500 group-hover:shadow-lg group-hover:shadow-cyan-500/50"></div>
                 <a href="#" title="" class="relative inline-flex items-center justify-center px-8 py-3 text-base font-normal text-white bg-black border border-transparent rounded-full" role="button"> Start Exploring Inspiration </a>
               </div>
