ed0795f33f5eaffa2fecc050cc2449d699132eea
Lai-Star <mclaistar@gmail.com>
2022-12-08 17:46:17 +0100

    feat: add new css hover effect

--- components/navbar.vue
+++ components/navbar.vue
@@ -188,4 +188,11 @@ export default {
 html {
   scroll-behavior: smooth;
 }
+a:hover {
+  color: #fff;
+  text-shadow: 0 0 5px #03e9f4,
+  0 0 25px #03e9f4,
+  0 0 50px #03e9f4,
+  0 0 100px #03e9f4;
+}
 </style>
